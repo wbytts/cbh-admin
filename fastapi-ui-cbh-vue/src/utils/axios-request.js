ee6c55c4c35cacc00af58306c640e4a380f118ba
wangbingyi <1041493283@qq.com>
2022-08-03 17:09:08 +0800

    update

--- fastapi-ui-cbh-vue/src/utils/axios-request.js
+++ fastapi-ui-cbh-vue/src/utils/axios-request.js
@@ -11,7 +11,8 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://39.96.162.54:8888/api/v1",
+  // baseURL: "http://39.96.162.54:8888/api/v1",
+  baseURL: "/api/v1",
   timeout: 6000, // 六秒不返回结果，报错（也可以重试）
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
--- fastapi-ui-cbh-vue/vue.config.js
+++ fastapi-ui-cbh-vue/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/cbh-admin',
+  // publicPath: '/cbh-admin',
   devServer: {
     watchFiles: {
       paths: ['src/**/*.*', 'public/**/*'],
